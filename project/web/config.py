1e9799416e3c844927ff337abab44a011f7660c3
Tim <ismagilovtr7118@gmail.com>
2020-01-12 16:03:05 +0300

    Первый коммит этого проекта

---
+++ project/web/config.py
@@ -0,0 +1,10 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'my_key_is_so_secret'
+# pagination
+RECORDS_PER_PAGE = 3
+# db params
+DATABASENAME="query_results_db" 
+USER="postgres"
+PASSWORD="123456"
+HOST="10.0.2.15"
+PORT="5432"
